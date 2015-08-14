5a0dcd8e6bb6b5d9397196a13ac31f7112b651c2
Mostafa Anter <ga30000@gmail.com>
2015-08-14 16:27:05 +0200

    Initial commit

---
+++ app/src/main/java/com/mostafaAnter/link_activities/Activity2.java
@@ -0,0 +1,15 @@
+package com.mostafaAnter.link_activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Mostafa on 8/14/2015.
+ */
+public class Activity2 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
